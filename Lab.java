54322d94d416237ffb2fc1a44aada25f827157eb
Priscigs <39099667+Priscigs@users.noreply.github.com>
2020-09-23 10:48:31 -0600

    Add files via upload
    Laberinto

---
+++ Lab.java
@@ -0,0 +1,49 @@
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class Lab extends JFrame{
+
+	Laberinto lab = new Laberinto();
+
+	Laberinto maze = new Laberinto();
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Lab lab = new Lab();
+					lab.setVisible(true);
+					lab.setSize(475, 335);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Lab() {
+		this.getContentPane().add(maze);
+		this.setResizable(true);
+		//setMinimumSize(new Dimension (475, 335));
+		//setBounds(100, 100, 475, 335);
+	}
+
+}
